25a8fa48f21f9c121415eec69c6df24b002c82ce
choseongchan3712 <choseongchan3712@naver.com>
2025-01-07 15:52:01 +0900

    study three complete

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -4,7 +4,7 @@ import BlogBox from "../components/BlogBox";
 import { useScrollTop } from "../lib/useScrollTop";
 
 const Container = styled.div`
-position: relative;
+  position: relative;
   z-index: 0;
   top: 0;
   left: 0;
@@ -14,7 +14,7 @@ position: relative;
   padding: 100px 15% 20px 15%;
   background-color: var(--main-color);
   overflow: hidden;
-  @media screen and (max-width: 1850px){
+  @media screen and (max-width: 1850px) {
     padding: 100vh 20px 20px 20px;
   }
   .wrap {
@@ -26,14 +26,19 @@ position: relative;
 `;
 
 const Blog = () => {
+
+
+
   useScrollTop();
-  return <Container>
-    <div className="wrap">
-      {blogData.map((data, index)=>(
-        <BlogBox key={index} url={data.url} text={data.text}/>
-      ))}
-    </div>
-  </Container>;
+  return (
+    <Container>
+      <div className="wrap">
+        {blogData.map((data, index) => (
+          <BlogBox key={index} url={data.url} text={data.text} />
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default Blog;
--- src/pages/Study.js
+++ src/pages/Study.js
@@ -1,5 +1,66 @@
+import { useEffect, useState } from "react";
+import { getGithub } from "../api/api";
+import styled from "styled-components";
+import StudyThree from "../components/StudyThree";
+import "aos/dist/aos.css";
+import Aos from "aos";
+
+const Container = styled.div`
+  position: relative;
+  z-index: 0;
+  width: 100%;
+  min-height: 100vh;
+  height: 100%;
+  background-color: var(--main-color);
+  padding: 100vh 15% 20px 15%;
+  .text_wrap {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+  }
+`;
+
 const Study = () => {
-  return <></>;
+  const [res, setRes] = useState();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getGithub();
+        setRes(
+          response?.data.map((data) => ({
+            name: data.name,
+            url: data.html_url,
+          }))
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+    Aos.init({
+      duration: 1200,
+      easing: "ease-in-out",
+      once: true,
+      offset: 300,
+    });
+  }, []);
+
+  return (
+    <Container>
+      <div className="text_wrap" data-aos="fade-up">
+        {res ? (
+          <StudyThree
+            wordList={res?.map((data) => data.name)}
+            to={res?.map((data) => data.url)}
+          />
+        ) : (
+          "loading"
+        )}
+      </div>
+    </Container>
+  );
 };
 
 export default Study;
